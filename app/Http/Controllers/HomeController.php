3d7b901c38056e8c8b484c7692bf89b7081036f7
Geass <dmjliar@gmail.com>
2018-10-09 13:15:12 +0800

    change var_dump

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,9 +14,10 @@ class HomeController extends Controller
     public function index()
     {
         $articles = \App\Article::all();
-       forEach ($articles as $article)
+        forEach ($articles as $article)
         {
-            var_dump($article->$title);
+            echo $article;
+            // var_dump($article->$title);
         }
         return view('home')->with('articles', \App\Article::all());
     }
