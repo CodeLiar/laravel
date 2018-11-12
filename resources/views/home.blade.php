8f7a4c24acd996ad625b1b85e7315c733aab0d61
root <geass>
2018-11-12 19:16:36 +0800

    delete element

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Learn Laravel 5</title>
-
     <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <script src="//cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
     <script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-
+<body>
     <div id="title" style="text-align: center;">
-        <h1>Learn Laravel 5</h1>
-        <div style="padding: 5px; font-size: 16px;">Learn Laravel 5</div>
     </div>
     <hr>
     <div id="content">
