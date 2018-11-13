53d6eefc7961d161ca2c661d28a70ac899135e9c
root <geass>
2018-11-13 13:43:03 +0800

    forms test finish

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,13 +1,10 @@
 <html>
 <head>
-<link rel='stylesheet' href="/css/bootstrap.min.css" type='text/css' media='all'/>
-<link rel='stylesheet' href="/css/all.css" type='text/css' media='all'/>
   <style type="text/css">
 
   </style>
 </head>
 <body>
-@section('content')
     <h1> 登录</h1>
     {!! Form::open() !!}
     <div class="form-group">
@@ -22,6 +19,5 @@
        {!! Form::submit('发表文章',['class'=>'btn btn-success form-control']) !!}
     </div>
     {!! Form::close() !!}
-@endsection
 </body>
 </html>
