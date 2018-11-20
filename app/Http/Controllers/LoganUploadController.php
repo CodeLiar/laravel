29e7de2d86ca11e58cde740256f594f4d53054fb
Geass <dmjliar@gmail.com>
2018-11-20 19:43:23 +0800

    request photo

--- app/Http/Controllers/LoganUploadController.php
+++ app/Http/Controllers/LoganUploadController.php
@@ -8,7 +8,17 @@ class LoganUploadController extends Controller
 {
     public function upload(Request $request)
     {
-        var_dump($request);
-        //return $request.get('body');
+        if ($request->hasFile('photo') && $request->file('photo')->isValid()) {
+            $photo = $request->file('photo');
+            $extension = $photo->extension();
+            //$store_result = $photo->store('photo');
+            $store_result = $photo->storeAs('photo', 'test.jpg');
+            $output = [
+                'extension' => $extension,
+                'store_result' => $store_result
+            ];
+            print_r($output);exit();
+        }
+        exit('未获取到上传文件或上传过程出错');
     }
 }
